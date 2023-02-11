55a03ea4f2d7d3373e710fef909fd1befccc4a82
ZorBoonraksa <boonraksa.buaraksa@gmail.com>
2023-02-12 02:50:24 +0700

    add conditions and array

--- Project1/app.js
+++ Project1/app.js
@@ -4,11 +4,22 @@ var lastName = "Buaraksa";
 for (let i = 0; i < 3; i++) {
   console.log(i + 1 + ". Hello " + Name + " : " + firstName + " " + lastName);
 }
-let number = prompt("choose a number(000-999) : ")
-document.getElementById("chose").innerHTML = number;
 var result = Math.floor(Math.random() * 1000)
+console.log(result)
 document.getElementById("result").innerHTML = result;
-if(number === result){
+var amount = prompt("choose amount of numbers to choose: ")
+const numbers = []
+for(let i = 0 ; i < amount; i++){
+    numbers[i] = prompt((i+1)+".choose a number(000-999) : ")
+}
+document.getElementById("chose").innerHTML = numbers;
+var check = false
+for(let i = 0 ; i < amount; i++){
+    if(numbers[i] == result){
+        check = true
+    }
+}
+if(check === true){
     document.getElementById("noti").innerHTML = "Lucky!!!";
 } else {
     document.getElementById("noti").innerHTML = "Try another time!!!";
